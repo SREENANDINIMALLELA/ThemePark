1b1ad8281ab6db6112b403fed9841217bee92aec
sreenandini <mallelasreenandini2@gmail.com>
2019-11-12 18:41:16 +0000

    added visitor age check in playground

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -9,10 +9,12 @@ import static org.junit.Assert.assertEquals;
 public class PlaygroundTest {
     Playground playground;
     Visitor visitor;
+    Visitor visitor1;
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
         visitor = new Visitor(17,162,500);
+        visitor1 = new Visitor(14,162,200);
     }
 
     @Test
@@ -32,6 +34,10 @@ public class PlaygroundTest {
     @Test
     public void checkAgeCriteria() {
         assertEquals(true, playground.isAllowedTo(visitor));
+    }
+    @Test
+    public void checkUnderAgeCriteria() {
+        assertEquals(false, playground.isAllowedTo(visitor1));
     }
 
 
